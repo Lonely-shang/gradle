b6ed0827c03aef2c5b4e85f3419f811c9668de28
Adam Murdoch <adam@gradle.com>
2019-12-19 14:52:22 +1100

    Allow the Gradle distribution to optionally be built using a Kotlin 1.3.70 EAP.

--- buildSrc/subprojects/kotlin-dsl/kotlin-dsl.gradle.kts
+++ buildSrc/subprojects/kotlin-dsl/kotlin-dsl.gradle.kts
@@ -3,10 +3,18 @@ dependencies {
     implementation(project(":configuration"))
     implementation(project(":build"))
 
-    api(kotlin("gradle-plugin"))
-    api(kotlin("stdlib-jdk8"))
-    api(kotlin("reflect"))
-    api(kotlin("compiler-embeddable"))
+    if (System.getProperty("kotlin-eap") != null) {
+        println("**** USING EAP KOTLIN VERSION")
+        api(kotlin("gradle-plugin", "1.3.70-eap-16"))
+        api(kotlin("stdlib-jdk8", "1.3.70-eap-16"))
+        api(kotlin("reflect", "1.3.70-eap-16"))
+        api(kotlin("compiler-embeddable", "1.3.70-eap-16"))
+    } else {
+        api(kotlin("gradle-plugin"))
+        api(kotlin("stdlib-jdk8"))
+        api(kotlin("reflect"))
+        api(kotlin("compiler-embeddable"))
+    }
 
     implementation("org.gradle.kotlin:gradle-kotlin-dsl-conventions:0.4.1")
     implementation("com.gradle.publish:plugin-publish-plugin:0.10.0")
